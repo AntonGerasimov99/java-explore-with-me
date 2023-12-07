3e7de9be5057a44321baf91e453f6e9a59fcee3c
Anton <gerasimovanton347@yandex.ru>
2023-12-08 02:34:36 +0500

    fix bugs

--- main-service/src/main/java/ru/practicum/MainServiceApp.java
+++ main-service/src/main/java/ru/practicum/MainServiceApp.java
@@ -2,11 +2,8 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import ru.practicum.client.StatsClient;
 
 @SpringBootApplication
-@Import(StatsClient.class)
 public class MainServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApp.class, args);
